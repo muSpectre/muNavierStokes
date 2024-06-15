bb06a38f244226d7f86325eb99d3b35b65893833
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2024-06-15 15:41:20 +0200

    ENH: Added `slice.py` utility to extract slices from simulation dumps

--- plot.py
+++ plot.py
@@ -3,23 +3,23 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from netCDF4 import Dataset
 
-file = Dataset('navier_stokes.nc', 'r')
+file = Dataset('navier_stokes_slice.nc', 'r')
 
-u_fcsxyz = file.variables['velocity']
+u_fxy = file.variables['velocity']
 
 fig = plt.figure()
-data = u_fcsxyz[0, 0, 0, :, :, 0]
+data = u_fxy[0]
 pmesh = plt.pcolormesh(data / np.std(data))
 
 
 # plt.colorbar()
 
 def animate(i):
-    data = u_fcsxyz[i, 0, 0, :, :, 0]
+    data = u_fxy[i]
     pmesh.set_array(data.flatten() / np.std(data))
     return pmesh
 
 
-ani = animation.FuncAnimation(fig, animate, frames=u_fcsxyz.shape[0], interval=100)
+ani = animation.FuncAnimation(fig, animate, frames=u_fxy.shape[0], interval=100)
 ffwriter = animation.FFMpegWriter(fps=10)
 ani.save('navier_stokes.mp4', writer=ffwriter)
---
+++ slice.py
@@ -0,0 +1,12 @@
+from netCDF4 import Dataset
+
+file = Dataset('navier_stokes.nc', 'r')
+
+with Dataset('navier_stokes_slice.nc', 'w') as slice:
+    slice.createDimension('frame', None)
+    slice.createDimension('nx', len(file.dimensions['nx']))
+    slice.createDimension('ny', len(file.dimensions['ny']))
+    slice.createVariable('velocity', 'f8', ('frame', 'nx', 'ny') )
+    for frame, u_csxyz in enumerate(file.variables['velocity']):
+        data = u_csxyz[0, :, :, 0]
+        slice.variables['velocity'][frame] = data
