8d22634e13ae55037aaa9632280c839934d506ea
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2026-06-13 23:16:27 +0200

    ENH: flit build system

--- muNavierStokes/__init__.py
+++ muNavierStokes/__init__.py
@@ -1,2 +1,8 @@
+"""Pseudo-spectral direct numerical simulation of the incompressible Navier-Stokes equations on top of muGrid."""
+
 from .NavierStokes import NavierStokes
 from .RungeKutta import rk4
+
+__version__ = "0.1.0"
+
+__all__ = ["NavierStokes", "rk4"]
